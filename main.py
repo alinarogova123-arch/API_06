73edc76141893f37c7584ee366ee63b4a637c680
alinarogova123-arch <alinarogova123@gmail.com>
2026-02-24 12:13:19 +0300

    Update main.py Переименовал переменные и функции

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ import requests
 from dotenv import load_dotenv
 
 
-FIRST_COMICS_NUMBER = 1
+FIRST_COMIC_NUMBER = 1
 
 
-def get_last_comics_number():
+def get_last_comic_number():
 	response = requests.get("https://xkcd.com/info.0.json")
 	response.raise_for_status()
-	last_comics_number = response.json().get("num")
+	last_comic_number = response.json().get("num")
 
-	return last_comics_number
+	return last_comic_number
 
 
-def get_comics_data(comics_number):
-	url = f"https://xkcd.com/{comics_number}/info.0.json"
+def get_comic_text_and_image_url(comic_number):
+	url = f"https://xkcd.com/{comic_number}/info.0.json"
 	response = requests.get(url)
 	response.raise_for_status()
-	comics_image_url = response.json().get("img")
-	comics_text = response.json().get("alt")
+	comic_image_url = response.json().get("img")
+	comic_text = response.json().get("alt")
 
-	return comics_image_url, comics_text
+	return comic_image_url, comic_text
 
 
-def upload_comics(url, token, chat_id, caption):
+def upload_comic(url, token, chat_id, caption):
 	bot = telegram.Bot(token=token)
 	bot.send_photo(chat_id=chat_id, photo=url, caption=caption)
 
@@ -36,9 +36,9 @@ def main():
 	load_dotenv(".env")
 	token = os.environ["TELEGRAM_BOT_API_KEY"]
 	chat_id = os.environ["TLGRM_CHAT_ID"]
-	comics_number = random.randint(FIRST_COMICS_NUMBER, get_last_comics_number())
-	comics_image_url, comics_text = get_comics_data(comics_number)
-	upload_comics(comics_image_url, token, chat_id, comics_text)
+	comic_number = random.randint(FIRST_COMIC_NUMBER, get_last_comic_number())
+	comic_image_url, comic_text = get_comic_text_and_image_url(comic_number)
+	upload_comic(comic_image_url, token, chat_id, comic_text)
 
 
 if __name__ == "__main__":
